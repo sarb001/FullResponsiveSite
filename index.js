a857664fdabb548c431c0bb846455a4336617438
SARB <mrsinghbusiness05@gmail.com>
2022-10-08 15:26:42 +0530

    Media Queries Started

---
+++ index.js
@@ -0,0 +1,17 @@
+// const headerbar = document.querySelector('#header-bar');
+
+// document.onscroll = () =>
+// {
+//     if(window.scrollY > 0) { headerbar.classList.add('active')}
+//     else {
+//         headerbar.classList.remove('active')
+//     }
+// }
+
+// document.onload = () =>
+// {
+//     if(window.scrollY > 0) { headerbar.classList.add('active')}
+//     else {
+//        headerbar.classList.remove('active')
+//     }
+// }
