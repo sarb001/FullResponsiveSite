cffed11c0a5161007b79cea50520f990b6939f73
SARB <mrsinghbusiness05@gmail.com>
2022-10-08 20:10:35 +0530

    Navbar Done

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-// const headerbar = document.querySelector('#header-bar');
-
-// document.onscroll = () =>
-// {
-//     if(window.scrollY > 0) { headerbar.classList.add('active')}
-//     else {
-//         headerbar.classList.remove('active')
-//     }
-// }
-
-// document.onload = () =>
-// {
-//     if(window.scrollY > 0) { headerbar.classList.add('active')}
-//     else {
-//        headerbar.classList.remove('active')
-//     }
-// }
 
 
 const pressbtn = document.querySelector('.ham-icon');
